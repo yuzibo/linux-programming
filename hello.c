3be4b2340766b45face4f2cf6222a49717c39a0b
YU BO <tsu.yubo@gmail.com>
2016-01-15 21:31:58 +0800

    内核编程的hello,world!

---
+++ hello.c
@@ -0,0 +1,37 @@
+#include<linux/module.h>
+#include<linux/kernel.h>
+
+MODULE_LICENSE("Dual BSD/GPL");
+static int hello_init(void)
+{
+	printk(KERN_INFO "Hello,World!\n");
+	return 0;
+}
+static void hello_exit(void)
+{
+	printk(KERN_INFO "exit sucessfully!\n");
+}
+
+/*
+ * initialize of modules
+ *
+ */
+module_init(hello_init);
+/**
+ * exit of modules
+ */
+module_exit(hello_exit);
+
+
+ifneq ($(KERNELRELEASE),)
+	obj-m := hello.o
+else
+	KDIR ?= /lib/modules/`uname -r`/build
+	CFLAGS_hello.o := -DDEBUG
+	PWD := $(shell pwd)
+default:
+	$(MAKE) -C $(KDIR) M=$$PWD
+clean:
+	make -C $(KDIR) M=$(PWD) clean
+
+endif
